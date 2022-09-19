4e8283dcc5f4dcfe9f307972bd923e6bb1392522
Bao <huynhlbg@mail.uc.edu>
2022-09-19 19:42:18 -0400

    Add Contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import About from './About.svelte';
 	import Skills from './Skills.svelte';
 	import Projects from './Projects.svelte';
+	import Contact from './Contact.svelte';
 </script>
 
 <Hero />
 <About />
 <Skills />
 <Projects />
+<Contact />
