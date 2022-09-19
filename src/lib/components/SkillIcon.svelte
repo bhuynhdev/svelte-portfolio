89b94785626e8b6f49ce2ce77752afdab527e564
Bao <huynhlbg@mail.uc.edu>
2022-09-18 20:54:52 -0400

    Add Skills section

---
+++ src/lib/components/SkillIcon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let iconName: string;
+	export let size: number;
+</script>
+
+<figure class="skillIcon">
+	<img src="/icons/{iconName.toLowerCase()}.svg" alt={iconName} width={size} height={size} />
+	<figcaption>{iconName}</figcaption>
+</figure>
+
+<style lang="scss">
+	.skillIcon {
+		text-align: center;
+		width: auto;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		row-gap: 0.35em;
+		figcaption {
+			font-size: 0.85rem;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Hero from './Hero.svelte';
 	import About from './About.svelte';
+	import Skills from './Skills.svelte';
 	import Projects from './Projects.svelte';
 </script>
 
 <Hero />
 <About />
+<Skills />
 <Projects />
