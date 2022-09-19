07fb5a4c6fbe810d23b664323b70d26f5fabf6e8
Bao <huynhlbg@mail.uc.edu>
2022-09-18 20:54:52 -0400

    Add Skills section

---
+++ src/routes/Skills.svelte
@@ -0,0 +1,88 @@
+<script>
+	import SkillIcon from '$lib/components/SkillIcon.svelte';
+</script>
+
+<section class="skills">
+	<h2>Skills</h2>
+	<div class="content-wrapper">
+		<div class="skill-category">
+			<div class="headline">
+				<h3>Frontend</h3>
+				<p>Creating good frontend</p>
+			</div>
+			<ul class="list">
+				{#each ['React', 'Angular', 'JavaScript', 'Typescript', 'HTML', 'SASS', 'Nextjs', 'TailwindCSS'] as skill}
+					<li class="skill-item">
+						<SkillIcon iconName={skill} size={40} />
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="skill-category">
+			<div class="headline">
+				<h3>Backend</h3>
+				<p>Creating scaleable backend</p>
+			</div>
+			<ul class="list">
+				{#each ['Nodejs', 'Express', 'NestJS', 'MongoDB', 'MySQL', 'Python', 'FastAPI', 'Flask'] as skill}
+					<li class="skill-item">
+						<SkillIcon iconName={skill} size={40} />
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="skill-category">
+			<div class="headline">
+				<h3>Developer tools</h3>
+				<p>use the right tool</p>
+			</div>
+			<ul class="list">
+				{#each ['Azure', 'Git', 'Github', 'Heroku', 'Jest', 'VsCode'] as skill}
+					<li class="skill-item">
+						<SkillIcon iconName={skill} size={40} />
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</section>
+
+<style lang="scss">
+	.content-wrapper {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 4em;
+		> * {
+			flex: 1;
+		}
+	}
+
+	.skill-category {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+
+	.list {
+		margin-top: 2em;
+		display: flex;
+		gap: 1.5em 2.25em;
+		flex-wrap: wrap;
+		justify-content: center;
+		.skill-item {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-radius: 12px;
+			transform: scale(1);
+			transition: transform 200ms;
+			&:hover {
+				transform: scale(1.15);
+			}
+		}
+	}
+</style>
