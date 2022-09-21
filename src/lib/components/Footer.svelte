46a184341f7ae5d3463a7c6bfd302cc4d643fe8a
Bao <huynhlbg@mail.uc.edu>
2022-09-21 01:01:30 -0400

    Add Footer section

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,76 @@
+<footer class="footer">
+	<nav class="nav">
+		<ul class="links">
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="#about">About</a>
+			</li>
+			<li>
+				<a href="#projects">Projects</a>
+			</li>
+			<li>
+				<a href="#contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="socials">
+		<ul>
+			<li>
+				<a href="https://github.com/bhuynhdev">
+					<img src="/icons/github.svg" alt="Github icon" width="32" />
+				</a>
+			</li>
+			<li>
+				<a href="https://linkedin.com/in/bhuynhuc">
+					<img src="/icons/linkedin.svg" alt="LinkedIn icon" width="32" />
+				</a>
+			</li>
+			<li>
+				<a href="https://twitter.com/bhuynhtweet">
+					<img src="/icons/twitter.svg" alt="Twitter icon" width="32" />
+				</a>
+			</li>
+			<li>
+				<a href="https://instagram.com">
+					<img src="/icons/instagram.svg" alt="Instagram icon" width="32" />
+				</a>
+			</li>
+		</ul>
+	</div>
+	<p>© 2022 Bao Huynh</p>
+</footer>
+
+<style lang="scss">
+	.footer {
+		background-color: #f8f8f8;
+		text-align: center;
+		padding: 0.5rem;
+		font-size: 1.1rem;
+	}
+
+	.nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	.links {
+		display: flex;
+		gap: 1em;
+
+		li a:hover {
+			color: #28afb0;
+		}
+	}
+
+	.socials {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		ul {
+			display: flex;
+			gap: 2em;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import '$lib/styles/global.scss';
 </script>
@@ -19,4 +20,4 @@
 <main>
 	<slot />
 </main>
-<footer />
+<Footer />
