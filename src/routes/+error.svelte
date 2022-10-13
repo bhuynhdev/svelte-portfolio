9cd670db69f33b040f5aee84fbd173238426d096
Bao <huynhlbg@mail.uc.edu>
2022-10-13 17:44:53 -0400

    Add 404 error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<section>
+	<h2>Error - 404 - Page not found</h2>
+	<p>You wandered into the unknown</p>
+	<p>Click <a class="home-link" href="/">here</a> to get back to the home page</p>
+</section>
+
+<style lang="scss">
+	section {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.home-link {
+		color: #058181;
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+</style>
