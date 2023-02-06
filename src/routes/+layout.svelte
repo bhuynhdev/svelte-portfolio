d4c37707bb883c3c9787c4339e66af11ec487976
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-02-05 23:12:46 -0500

    Self host fontawesome CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
@@ -18,7 +19,9 @@
 		href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:wght@400;500;600;700;800;900&display=swap"
 		rel="stylesheet"
 	/>
-	<script src="https://kit.fontawesome.com/63bc15acb0.js" crossorigin="anonymous"></script>
+	<link href="/fontawesome6/css/fontawesome.min.css" rel="stylesheet" />
+	<link href="/fontawesome6/css/brands.min.css" rel="stylesheet" />
+	<link href="/fontawesome6/css/solid.min.css" rel="stylesheet" />
 </svelte:head>
 
 <Header />
